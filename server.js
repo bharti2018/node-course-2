010d2c365ec6894f1a6b19c48162c5656e3bb546
Abhishek Kumar Bharti -X (abbharti - WIPRO LIMITED at Cisco) <abbharti@partnet.cisco.com>
2017-06-04 14:45:41 +0530

    'Initial'

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine','hbs');
+
+// app.use((req,res,next) => {
+//   res.render('maintenence.hbs');
+//
+// });
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.use(express.static(__dirname +'/public'));
+app.use((req,res,next) => {
+  var now = new Date().toString();
+  // console.log(`${now}: ${req.method} ${req.url}`)
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log +'\n', (err) =>{
+    if(err){
+      console.log('unable to append File server.log');
+    }
+  } );
+next();
+});
+
+app.get('/',(req,res) => {
+  res.render('home.hbs', {
+    Pagetitle: 'Welcome Page',
+    welcomeMessage: 'welcome to my site',
+    // currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/about',(req,res) => {
+  res.render('about.hbs', {
+    Pagetitle: 'About Page',
+    // currentYear: new Date().getFullYear()
+  });
+});
+
+app.listen(3000);
